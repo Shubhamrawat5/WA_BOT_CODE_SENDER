f57416b8dc0218fcff0fff707d19eba767373887
Shubham Rawat <sr856161@gmail.com>
2020-11-24 01:26:02 +0530

    fixed send message

--- Get_code.py
+++ Get_code.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as bs
 import time
 import keyboard
 
-q='count set bits' #program name
+q='reverse a string' #program name
 
 url='https://www.google.com/search?q='
 
@@ -16,25 +16,31 @@ desiredLink=soup.find("div",class_='kCrYT').a['href']  #get first top link
 response=requests.get('https://www.google.com'+desiredLink)
 
 soup=bs(response.content,"lxml")
-code=soup.find("div",class_='code-block').text #get code in gfg page
-code=code.replace(u'\xa0', u' ') #there are many \x0 in code so replacing all with space
-code=code[79:] #slicing unnecessary part in front
+inner=soup.find("div",class_='code-block')
+container=inner.find("div",class_='container')
+lines=container.findAll("div")
+l=[]
+for line in lines:
+    txt=line.text
+    txt=txt.replace(u'\xa0', u' ')
+    print(txt)
+    l.append(txt)
+
+#print(l)
+code="\n".join(l)
+print(code)
 
 # NOTE: using keyboard library to send the code text, uncomment below lines to send the code to post somewhere automatically
 # time.sleep(2)
-# for i in code:
+# for i in l:
 #     #print(i)
-#     if i != '\n': #\n is treated as enter, so if \n come then it will instead of breaking link, it will send the message.. so handling it here
-#         keyboard.write(i)
-#     else:
-#         keyboard.press_and_release('shift+enter')
+#     keyboard.write(i)
+#     keyboard.press_and_release('shift+enter')
 
 # keyboard.press_and_release('enter') #send the message(code) written
 
-print(code)
-
 #saving the code in a file name code.txt
-r=open(r'C:\Users\Shubham\Desktop\Program\code.txt','w',encoding='utf-8')
-r.write(code)
-print("Code saved in code.txt file locally!")
-r.close()
+# r=open(r'C:\Users\Shubham\Desktop\Program\code.txt','w',encoding='utf-8')
+# r.write(code)
+# print("Code saved in code.txt file locally!")
+# r.close()
--- WA_GFGcode_sender.py
+++ WA_GFGcode_sender.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import keyboard
+from selenium.webdriver.common.keys import Keys #for keyboard keys pressing like for next line = shift+enter
 from selenium import webdriver
 import time
 
@@ -27,70 +27,76 @@ def refresh():
     try:
         outer=driver.find_element_by_css_selector('div[class="copyable-text"]')
         inner=outer.find_element_by_css_selector('div[dir="ltr"]')
-        inner.get_attribute("class")
         msg_class=inner.get_attribute("class")
     except:
-        outer=driver.find_element_by_css_selector('div[class="cln10 copyable-text"]')
-        inner=outer.find_element_by_css_selector('div[dir="ltr"]')
-        inner.get_attribute("class")
-        msg_class=inner.get_attribute("class")
-    
-    #total chats in current chat
-    chats=driver.find_elements_by_class_name(msg_class)
+        try:
+            outer=driver.find_element_by_css_selector('div[class="cln10 copyable-text"]')
+            inner=outer.find_element_by_css_selector('div[dir="ltr"]')
+            msg_class=inner.get_attribute("class")
+        except:
+            print("Error in classname!!!!")
+            exit()
 
-    #get last message in total chat
     try:
+        #total chats in current chat
+        chats=driver.find_elements_by_class_name(msg_class)
+
+        #get last message in total chat
         msg=chats[-1].text
         print("Message is:"+msg)
     except:
         print("Either chat is empty or messages class name is incorrect !")
+        exit()
 
     try:
         if msg[0:4]=='#gfg': # query asked!
-            q=msg[5:]
+            q=msg[5:] 
             print("QUERY FOUND !\nProgram to be search is "+q)
 
             url='https://www.google.com/search?q='
 
-            response=requests.get(url+q+'+gfg')
+            response=requests.get(url+q+'+gfg') #searching in google
 
             soup=bs(response.content,"lxml")
 
-            desiredLink=soup.find("div",class_='kCrYT').a['href']
+            desiredLink=soup.find("div",class_='kCrYT').a['href'] #get first top link, gfg link it would be!
 
             g='https://www.google.com'
 
-            response=requests.get('https://www.google.com'+desiredLink)
+            response=requests.get('https://www.google.com'+desiredLink) #open link of gfg
 
-            # xpath='/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]' #path of the typing box of whatsapp chat
-            # msgbox=driver.find_element_by_xpath(xpath)
-            # msgbox.click()
-
+            #scraping asked program code
             soup=bs(response.content,"lxml")
-            code=soup.find("div",class_='code-block').text
-            code=code.replace(u'\xa0', u' ')
-            code=code[79:]
+            inner=soup.find("div",class_='code-block')
+            cont=inner.find("div",class_='container')
+            lines=cont.findAll("div")
 
-            time.sleep(2)
+            #a list is creating of all the lines of program code
+            l=[]
+            for line in lines:
+                txt=line.text
+                txt=txt.replace(u'\xa0', u' ')
+                #print(txt)
+                l.append(txt)
 
-            for i in code:
-                #print(i)
-                if i != '\n':
-                    keyboard.write(i)
-                else:
-                    keyboard.press_and_release('shift+enter')
+            xpath='/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]' #path of the typing box of whatsapp chat
+            msgbox=driver.find_element_by_xpath(xpath)
 
-            time.sleep(1)
-            keyboard.press_and_release('enter')
+            #now sending the program code to message box of whatsapp automatically
+            for line in l:
+                if line=='':
+                    continue
+                msgbox.send_keys(line)
+                msgbox.send_keys(Keys.SHIFT+Keys.ENTER)
 
-            #print(code)
-            time.sleep(1)
+            msgbox.send_keys(Keys.ENTER)
+            print("-------------------\n PROGRAM SENT SUCCESSFULLY ! \n-------------------")
     except:
-        print("ERROR")
+        print("ERROR IN GETTING PROGRAM !")
 
 
 time.sleep(4)
 while True:
     print("-----SCANNING MESSAGES!!!-----")
     refresh()
-    time.sleep(3)
+    time.sleep(2)
